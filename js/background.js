993b2cf39582cfb6631c7169c632c23f1e89d796
wlals111 <limjimjn514@gmail.com>
2025-06-18 22:07:13 +0900

    UPDATE : 배경 이미지 설정 방식 변경 및 img 태그 삽입 제거

--- js/background.js
+++ js/background.js
@@ -5,9 +5,8 @@ const imgs = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
 // 랜덤 img 가져오기
 const randomImg = imgs[Math.floor(Math.random() * imgs.length)];
 
-// img태그 추가 + src속성
-const $img = document.createElement('img');
-$img.setAttribute('src', `./img/${randomImg}`);
-
-// body태그 안에 img태그 삽입
-$body.appendChild($img);
+// 배경 스타일 설정
+document.body.style.backgroundImage = `url(../img/${randomImg})`;
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backdropFilter = 'blur(2px)';
